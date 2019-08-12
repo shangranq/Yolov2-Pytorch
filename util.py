f7b514f7d1a2f315be1168c74029671e1824d806
shangranq <52607857+shangranq@users.noreply.github.com>
2019-08-12 14:13:58 -0400

    Update util.py

--- util.py
+++ util.py
@@ -6,14 +6,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 from tqdm import tqdm
 
-class BoundBox:
-    def __init__(self, xmin, xmax, ymin, ymax, obj_type):
-        self.xmin = xmin
-        self.ymin = ymin
-        self.xmax = xmax
-        self.ymax = ymax
-        self.obj_type = obj_type 
-
 
 def parse_annotation(ann_dir, img_dir, labels=[]):
     all_imgs = []
@@ -63,6 +55,116 @@ def parse_annotation(ann_dir, img_dir, labels=[]):
             all_imgs += [img]
                         
     return all_imgs, seen_labels
+
+
+def bbox_iou(box1, box2):
+    intersect_w = _interval_overlap([box1.xmin, box1.xmax], [box2.xmin, box2.xmax])
+    intersect_h = _interval_overlap([box1.ymin, box1.ymax], [box2.ymin, box2.ymax])  
+    intersect = intersect_w * intersect_h
+    w1, h1 = box1.xmax-box1.xmin, box1.ymax-box1.ymin
+    w2, h2 = box2.xmax-box2.xmin, box2.ymax-box2.ymin
+    union = w1*h1 + w2*h2 - intersect
+    return float(intersect) / union
+
+
+def draw_boxes(image, boxes, labels):
+    image_h, image_w, _ = image.shape
+
+    for box in boxes:
+        xmin = int(box.xmin*image_w)
+        ymin = int(box.ymin*image_h)
+        xmax = int(box.xmax*image_w)
+        ymax = int(box.ymax*image_h)
+        a, b, c = np.random.randint(0, 255), np.random.randint(0, 255), np.random.randint(0, 255)
+        cv2.rectangle(image, (xmin,ymin), (xmax,ymax), (a, b, c), 3)
+        cv2.putText(image, 
+                    labels[box.get_label()] + ' ' + str(box.get_score()), 
+                    (xmin, ymin - 13), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 
+                    1e-3 * image_h, 
+                    (0,255,0), 2)
+    return image
+
+
+def compute_overlap(a, b):
+    """
+    Code originally from https://github.com/rbgirshick/py-faster-rcnn.
+    Parameters
+    ----------
+    a: (N, 4) ndarray of float
+    b: (K, 4) ndarray of float
+    Returns
+    -------
+    overlaps: (N, K) ndarray of overlap between boxes and query_boxes
+    """
+    area = (b[:, 2] - b[:, 0]) * (b[:, 3] - b[:, 1])
+    iw = np.minimum(np.expand_dims(a[:, 2], axis=1), b[:, 2]) - np.maximum(np.expand_dims(a[:, 0], 1), b[:, 0])
+    ih = np.minimum(np.expand_dims(a[:, 3], axis=1), b[:, 3]) - np.maximum(np.expand_dims(a[:, 1], 1), b[:, 1])
+
+    iw = np.maximum(iw, 0)
+    ih = np.maximum(ih, 0)
+
+    ua = np.expand_dims((a[:, 2] - a[:, 0]) * (a[:, 3] - a[:, 1]), axis=1) + area - iw * ih
+
+    ua = np.maximum(ua, np.finfo(float).eps)
+
+    intersection = iw * ih
+
+    return intersection / ua  
+
+
+def compute_ap(recall, precision):
+    """ Compute the average precision, given the recall and precision curves.
+    Code originally from https://github.com/rbgirshick/py-faster-rcnn.
+    # Arguments
+        recall:    The recall curve (list).
+        precision: The precision curve (list).
+    # Returns
+        The average precision as computed in py-faster-rcnn.
+    """
+    # correct AP calculation
+    # first append sentinel values at the end
+    mrec = np.concatenate(([0.], recall, [1.]))
+    mpre = np.concatenate(([0.], precision, [0.]))
+
+    # compute the precision envelope
+    for i in range(mpre.size - 1, 0, -1):
+        mpre[i - 1] = np.maximum(mpre[i - 1], mpre[i])
+
+    # to calculate area under PR curve, look for points
+    # where X axis (recall) changes value
+    i = np.where(mrec[1:] != mrec[:-1])[0]
+
+    # and sum (\Delta recall) * prec
+    ap = np.sum((mrec[i + 1] - mrec[i]) * mpre[i + 1])
+    return ap  
+
+        
+def _interval_overlap(interval_a, interval_b):
+    x1, x2 = interval_a
+    x3, x4 = interval_b
+    if x3 < x1:
+        if x4 < x1:
+            return 0
+        else:
+            return min(x2,x4) - x1
+    else:
+        if x2 < x3:
+             return 0
+        else:
+            return min(x2,x4) - x3          
+
+def _sigmoid(x):
+    return 1. / (1. + np.exp(-x))
+
+def _softmax(x, axis=-1, t=-100.):
+    x = x - np.max(x)
+    if np.min(x) < t:
+        x = x/np.min(x)*t
+    e_x = np.exp(x)
+    return e_x / e_x.sum(axis, keepdims=True)
+
+
 
 
 def load_image(filename):
